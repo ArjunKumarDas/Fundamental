1d95a1645e399717be6173a37e9fa0d633ebf79f
ArjunKumarDas <arjunkdinfo@gamil.com>
2020-11-05 12:20:30 +0600

    reduce

---
+++ reduce.js
@@ -0,0 +1,30 @@
+var years = [1,2,3,4]
+const total = years.reduce((acc, n) => acc + n, 0);
+console.log(total);
+
+
+var pilots = [
+    {
+      id: 10,
+      name: "Poe Dameron",
+      years: 14,
+    },
+    {
+      id: 2,
+      name: "Temmin 'Snap' Wexley",
+      years: 30,
+    },
+    {
+      id: 41,
+      name: "Tallissan Lintra",
+      years: 16,
+    },
+    {
+      id: 99,
+      name: "Ello Asty",
+      years: 22,
+    }
+  ];
+
+  const totalYears = pilots.reduce((acc, pilot) => acc + pilot.years, 0);
+  console.log(totalYears);
